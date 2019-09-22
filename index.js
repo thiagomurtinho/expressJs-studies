4118bf587e5b91c72f6b2aa61e75402848da22d2
Thiago Murtinho <thiago.murtinho@live.com>
2019-09-22 11:22:40 -0300

    Feature: firts route

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express'
 // import { process } from 'ipaddr.js';
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Word</h1>')
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`))
