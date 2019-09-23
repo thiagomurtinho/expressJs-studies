76dbec52470907fbf3f42e68f3eb6c2881cc4354
Thiago Murtinho <thiago.murtinho@live.com>
2019-09-22 21:35:54 -0300

    Feature: Fix res to inexisting id get

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(logger)
 //Get all members
 app.get('/api/members', (req, res) => res.json(members))
 
+//Get single member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if(found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id))) //Estudar!!
+    } else {
+        res.status(400).json({ msg: `ID(${req.params.id}) not found` })
+    }
+})
+
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
