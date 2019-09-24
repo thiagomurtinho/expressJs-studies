158c7aaef826ef2582c04a9ae21ff245107ec9b7
Thiago Murtinho <thiago.murtinho@live.com>
2019-09-23 22:41:51 -0300

    Feature: Update member

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import uuid from 'uuid'
 
 import members from '../../Members'
 
@@ -14,6 +15,43 @@ router.get('/:id', (req, res) => {
 
     if(found) {
         res.json(members.filter(member => member.id === parseInt(req.params.id))) //Estudar!!
+    } else {
+        res.status(400).json({ msg: `ID(${req.params.id}) not found` })
+    }
+})
+
+//Create members
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    if(!newMember.name || !newMember.email) {
+        res.status(400).json({ msg: "Please include a name and email"  })
+    }
+
+    members.push(newMember)
+    res.json(members)
+})
+
+//Update a member
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if(found) {
+        const updMember = req.body
+            console.log(`body request: ${updMember}`)
+        members.forEach(member => {
+            if(member.id === parseInt(req.params.id)) {
+                member.name = updMember.name ? updMember.name : member.name
+                member.email = updMember.email ? updMember.email : member.email
+
+                res.json({ msg: 'Member updated', member })
+            }
+        })
     } else {
         res.status(400).json({ msg: `ID(${req.params.id}) not found` })
     }
